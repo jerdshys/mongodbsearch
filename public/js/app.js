af4705f33e7f3ef04247ea38119b22a3ad5f707b
jerdshys <deshayesch@eisti.eu>
2014-12-07 17:14:44 +0100

    gallerie+debut upload

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 
 // Declare app level module which depends on filters, and services
 
-var app = angular.module('myApp', ['myApp.filters', 'myApp.directives']).
+var app = angular.module('myApp', ['myApp.filters', 'myApp.directives','angularFileUpload']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/', {
--- public/js/controllers/food.js
+++ public/js/controllers/food.js
@@ -2,8 +2,10 @@
 
 function AddFoodCtrl($scope, $http, $location) {
    
+  // formulaire simple
   $scope.form = {};
-    $scope.addFood = function () {
+  $scope.fileForm = {};
+  $scope.addFood = function () {
           console.log("launching HTTP POST");
           $http.post('/api/post', $scope.form).success(function(data) {
             alert("post success");
@@ -16,9 +18,34 @@ function AddFoodCtrl($scope, $http, $location) {
 
    } 
 
-    $http.get('/api/get').success(function(data, status, headers, config) {
+   // get data from server
+  $http.get('/api/get').success(function(data, status, headers, config) {
           $scope.catalogue = data.foods;
-    });
+  });
+
+  $scope.addFile = function () {
+    alert("lol");
+    alert($scope.fileForm.files);
+          console.log($scope.fileForm.file);
+
+        }
 
   
 }
+/*
+var MyCtrl = [ '$scope', '$upload', function($scope, $upload) {
+  $scope.onFileSelect = function($files) {
+    //$files: an array of files selected, each file has name, size, and type.
+    for (var i = 0; i < $files.length; i++) {
+      var $file = $files[i];
+      $upload.upload({
+        url: 'my/upload/url',
+        file: $file,
+        progress: function(e){}
+      }).then(function(data, status, headers, config) {
+        // file is uploaded successfully
+        console.log(data);
+      }); 
+    }
+  }
+}];*/
